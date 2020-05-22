e8ec61a44279835289ab5ff3dc0a12076908c5b9
Steffen Schlaer <steffen.schlaer@it-cru.de>
2020-05-22 16:03:22 +0200

    task: cleanup popup.ts file

--- src/popup.ts
+++ src/popup.ts
@@ -11,15 +11,6 @@ import './account/limits';
 let count = 0;
 
 $(function() {
-  // const queryInfo = {
-  //   active: true,
-  //   currentWindow: true
-  // };
-
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   $('#url').text(tabs[0].url);
-  //   $('#time').text(moment().format('YYYY-MM-DD HH:mm:ss'));
-  // });
 
   jsrender($);
 
