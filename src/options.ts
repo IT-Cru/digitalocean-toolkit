2816e1146caecbee8f6912554bc4d60f8ca57240
Steffen Schlaer <steffen.schlaer@it-cru.de>
2020-05-28 23:47:13 +0200

    task: cleanup options dialog and add event tracking

--- src/options.ts
+++ src/options.ts
@@ -3,39 +3,32 @@ import {tracking} from './utils/tracking/tracking';
 
 // Saves options to chrome.storage.sync.
 function save_options() {
-  var color = $('#color').val();
-  var likesColor = $('#like').prop('checked');
-  var apiAccessToken = $('#api_access_token').val();
+  tracking.event('Options', 'Click', { eventLabel: 'Save options' });
+
+  let apiAccessToken = $('#api_access_token').val();
   chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor,
     apiAccessToken: apiAccessToken
   }, function() {
+
     // Update status to let user know options were saved.
-    var status = $('#status');
+    let status = $('#status');
     status.text('Options saved.');
     setTimeout(function() {
       status.text('');
-    }, 750);
+    }, 1500);
   });
 }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
+// Saves options from chrome.storage.sync.
 function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true,
     apiAccessToken: ''
-  }, function(items: {favoriteColor, likesColor, apiAccessToken}) {
-    $('#color').val(items.favoriteColor);
-    $('#like').prop('checked', items.likesColor);
+  }, function(items: {apiAccessToken}) {
     $('#api_access_token').val(items.apiAccessToken);
   });
 }
 
 tracking.pageview('/options');
 
-$('#save').click(save_options);
+$('#save').on('click', save_options);
 $(restore_options); // document.addEventListener('DOMContentLoaded', restore_options);
