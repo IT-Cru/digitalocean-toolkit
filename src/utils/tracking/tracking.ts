8ee0df56781da6b53c3f368c5e1762eca45a1ef8
Steffen Schlaer <steffen.schlaer@it-cru.de>
2020-06-15 22:20:06 +0200

    fix: optimize tracking for dev and prod env

--- src/utils/tracking/tracking.ts
+++ src/utils/tracking/tracking.ts
@@ -1,11 +1,13 @@
 let analytics = require('universal-ga');
 
 let options = {
-    debug: false,
-    anonymizeIp: true
+    cookieDomain: 'dotoolkit.it-cru.de',
+    debug: process.env.NODE_ENV == 'development',
 };
 
 analytics.initialize('UA-67059303-4', options);
+analytics.set('anonymizeIp', true);
 analytics.set('checkProtocolTask', null);
+analytics.custom('dimension01', process.env.NODE_ENV);
 
 export const tracking = analytics;
